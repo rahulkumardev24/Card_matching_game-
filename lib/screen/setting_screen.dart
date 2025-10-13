a43de58b9a9a2e1ea1d65480e8b4a22b217e1fa6
rahul <rkrahulroy151617@gmail.com>
2025-10-13 19:24:58 +0530

    audio and flip animation apply

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,3 +1,5 @@
+import 'package:card_match_memory/helper/app_color.dart';
+import 'package:card_match_memory/helper/app_text_styles.dart';
 import 'package:card_match_memory/provider/setting_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,18 +11,24 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final settingProvider = Provider.of<SettingProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
+      appBar: AppBar(title: const Text('Settings')),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
           Card(
             child: ListTile(
-              leading: const Icon(Icons.volume_up),
-              title: const Text('Sound Effects'),
+              leading: Icon(Icons.volume_up, color: AppColor.darkText),
+              title: Text(
+                'Sound Effects',
+                style: AppTextStyle.subtitleSmall(
+                  color: AppColor.darkText,
+                  weight: FontWeight.bold,
+                ),
+              ),
               trailing: Switch(
                 value: settingProvider.isSoundOn,
+                activeTrackColor: AppColor.primaryColor,
+
                 onChanged: (value) {
                   settingProvider.toggleSound();
                 },
@@ -29,30 +37,17 @@ class SettingsScreen extends StatelessWidget {
           ),
           Card(
             child: ListTile(
-              leading: const Icon(Icons.music_note),
-              title: const Text('Background Music'),
-              trailing: Switch(
-                value: false,
-                onChanged: (value) {},
+              leading: Icon(Icons.vibration, color: AppColor.darkText),
+              title: Text(
+                'Vibration',
+                style: AppTextStyle.subtitleSmall(
+                  color: AppColor.darkText,
+                  weight: FontWeight.bold,
+                ),
               ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: const Icon(Icons.vibration),
-              title: const Text('Vibration'),
               trailing: Switch(
                 value: true,
-                onChanged: (value) {},
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: const Icon(Icons.timer),
-              title: const Text('Timer Display'),
-              trailing: Switch(
-                value: true,
+                activeTrackColor: AppColor.primaryColor,
                 onChanged: (value) {},
               ),
             ),
